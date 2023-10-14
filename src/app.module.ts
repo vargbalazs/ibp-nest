4a342ae15954055f6596223022e7517ada7414ce
vargbalazs <balazs.varga.8564@gmail.com>
2023-10-14 14:40:17 +0200

    working on user entity 3

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppConfigModule } from './config/app/config.module';
 import { PostgresProviderModule } from './providers/database/postgres/provider.module';
+import { UsersModule } from './models/users/users.module';
 
 @Module({
-  imports: [AppConfigModule, PostgresProviderModule],
+  imports: [AppConfigModule, PostgresProviderModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
