cdce01dd99d551ab033d35f20b5d7450e830fc00
vargbalazs <balazs.varga.8564@gmail.com>
2023-10-19 14:37:32 +0200

    adding api for cronjob

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  callFromCronJob() {
+    console.log('app called from cron job');
   }
 }
