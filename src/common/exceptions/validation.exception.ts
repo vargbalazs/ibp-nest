6a793e2ed0dcf93dbec3c1338f74cd7fdf383899
vargbalazs <balazs.varga.8564@gmail.com>
2023-10-26 08:51:51 +0200

    correct validation exception handler

--- src/common/exceptions/validation.exception.ts
+++ src/common/exceptions/validation.exception.ts
@@ -5,7 +5,7 @@ export const validationExceptionFactory = (errors: ValidationError[]) => {
   const formatError = (errors: ValidationError[]) => {
     const errMsg = {};
     errors.forEach((error: ValidationError) => {
-      errMsg[error.property] = error.children.length
+      errMsg[error.property] = error.children?.length
         ? [formatError(error.children)]
         : [...Object.values(error.constraints)];
     });
