3dd766d635d3271311da5d63fb7d70d9f5671e9f
vargbalazs <balazs.varga.8564@gmail.com>
2023-11-09 15:19:41 +0100

    adding relation action - permission

--- src/models/permissions/entities/permission.entity.ts
+++ src/models/permissions/entities/permission.entity.ts
@@ -1,13 +1,16 @@
 import {
   Column,
   Entity,
+  JoinColumn,
   ManyToMany,
   ManyToOne,
+  OneToOne,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 import { Permission } from '../interfaces/permission.interface';
 import { OperationModel } from 'src/models/operations/entities/operation.entity';
 import { RoleModel } from 'src/models/roles/entities/role.entity';
+import { ActionModel } from 'src/models/actions/entities/action.entity';
 
 @Entity({ name: 'permissions', synchronize: false })
 export class PermissionModel implements Permission {
@@ -24,4 +27,8 @@ export class PermissionModel implements Permission {
 
   @ManyToMany(() => RoleModel, (role) => role.permissions)
   roles: RoleModel[];
+
+  @OneToOne(() => ActionModel)
+  @JoinColumn()
+  action: ActionModel;
 }
