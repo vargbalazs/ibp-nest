ac894d89f2edb5e438e42f53eb7a867527a9de3e
vargbalazs <balazs.varga.8564@gmail.com>
2023-11-14 12:01:13 +0100

    extending update user dto

--- src/models/users/dto/update-user.dto.ts
+++ src/models/users/dto/update-user.dto.ts
@@ -1,8 +1,20 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends CreateUserDto {
   @IsNotEmpty()
   @IsString()
   userId: string;
+
+  @IsNotEmpty()
+  @IsBoolean()
+  active: boolean;
+
+  @IsNotEmpty()
+  @IsBoolean()
+  notLocked: boolean;
+
+  @IsNotEmpty()
+  @IsString()
+  currentUserName: string;
 }
