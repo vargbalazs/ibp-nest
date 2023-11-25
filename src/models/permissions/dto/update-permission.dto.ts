b54f54bfd708a3a95cfb2ea77b72e4ef777b9042
vargbalazs <balazs.varga.8564@gmail.com>
2023-11-25 08:11:33 +0100

    changes to permission dto

--- src/models/permissions/dto/update-permission.dto.ts
+++ src/models/permissions/dto/update-permission.dto.ts
@@ -1,8 +1,41 @@
-import { IsNotEmpty, IsNumber } from 'class-validator';
-import { CreatePermissionDto } from './create-permission.dto';
+import { Type } from 'class-transformer';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+import { UpdateActionDto } from 'src/models/actions/dto/update-action.dto';
+import { SimpleModuleDto } from 'src/models/modules/dto/simple-module.dto';
+import { OperationForPermissionDto } from 'src/models/operations/dto/operation-for-permission.dto';
+import { UpdateSubModuleDto } from 'src/models/sub-modules/dto/update-sub-module.dto';
 
-export class UpdatePermissionDto extends CreatePermissionDto {
+export class UpdatePermissionDto {
   @IsNotEmpty()
   @IsNumber()
   id: number;
+
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @ValidateNested()
+  @Type(() => OperationForPermissionDto)
+  operation: OperationForPermissionDto;
+
+  @IsNotEmpty()
+  @ValidateNested()
+  @Type(() => UpdateActionDto)
+  action: UpdateActionDto;
+
+  @IsNotEmpty()
+  @ValidateNested()
+  @Type(() => SimpleModuleDto)
+  module: SimpleModuleDto;
+
+  @IsNotEmpty()
+  @ValidateNested()
+  @Type(() => UpdateSubModuleDto)
+  subModule: UpdateSubModuleDto;
 }
