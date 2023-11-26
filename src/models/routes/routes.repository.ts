6c289df372e961d8becaae9e1f814bb68f126d5b
vargbalazs <balazs.varga.8564@gmail.com>
2023-11-26 14:20:29 +0100

    finishing routes

--- src/models/routes/routes.repository.ts
+++ src/models/routes/routes.repository.ts
@@ -1,7 +1,24 @@
 import { RouteModel } from './entities/route.entity';
 import { RouteModelRepository } from './interfaces/repository.interface';
 import { TypeOrmRepository } from '../type-orm.repository';
+import { FindOptionsWhere } from 'typeorm';
 
 export class RouteRepository
   extends TypeOrmRepository<RouteModel>
-  implements RouteModelRepository {}
+  implements RouteModelRepository
+{
+  override async findAll(): Promise<RouteModel[]> {
+    return this.find({
+      relations: { roleGroups: true },
+      loadEagerRelations: false,
+    });
+  }
+
+  override async findById(id: number): Promise<RouteModel> {
+    return this.findOne({
+      where: { id: id },
+      relations: { roleGroups: true },
+      loadEagerRelations: false,
+    });
+  }
+}
