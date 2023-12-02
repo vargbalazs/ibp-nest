da35863f2a9ad05b804083b8fabacf484fb37f28
vargbalazs <balazs.varga.8564@gmail.com>
2023-12-02 18:11:51 +0100

    cleaning up models module (admin)

---
+++ src/models/admin/admin.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { RoleGroupsModule } from './role-groups/role-groups.module';
+import { UsersModule } from './users/users.module';
+import { ActionsModule } from './actions/actions.module';
+import { ModulesModule } from './modules/modules.module';
+import { OperationsModule } from './operations/operations.module';
+import { RolesModule } from './roles/roles.module';
+import { SubModulesModule } from './sub-modules/sub-modules.module';
+import { PermissionsModule } from './permissions/permissions.module';
+import { RoutesModule } from './routes/routes.module';
+
+@Module({
+  imports: [
+    UsersModule,
+    RoleGroupsModule,
+    ActionsModule,
+    ModulesModule,
+    OperationsModule,
+    RolesModule,
+    SubModulesModule,
+    PermissionsModule,
+    RoutesModule,
+  ],
+})
+export class AdminModule {}
--- src/models/models.module.ts
+++ src/models/models.module.ts
@@ -1,27 +1,8 @@
 import { Module } from '@nestjs/common';
-import { RoleGroupsModule } from './admin/role-groups/role-groups.module';
-import { UsersModule } from './admin/users/users.module';
-import { ActionsModule } from './admin/actions/actions.module';
-import { ModulesModule } from './admin/modules/modules.module';
-import { OperationsModule } from './admin/operations/operations.module';
-import { RolesModule } from './admin/roles/roles.module';
-import { SubModulesModule } from './admin/sub-modules/sub-modules.module';
-import { PermissionsModule } from './admin/permissions/permissions.module';
-import { RoutesModule } from './admin/routes/routes.module';
 import { BusModule } from './bu/bus.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [
-    UsersModule,
-    RoleGroupsModule,
-    ActionsModule,
-    ModulesModule,
-    OperationsModule,
-    RolesModule,
-    SubModulesModule,
-    PermissionsModule,
-    RoutesModule,
-    BusModule,
-  ],
+  imports: [AdminModule, BusModule],
 })
 export class ModelsModule {}
